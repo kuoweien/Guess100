945b4b4f38bbde85e257fef40bd3bdb31a50e663
WayneKuo <aasdfangela@gmail.com>
2017-11-14 10:45:37 +0800

    Guess

--- app/src/main/java/com/wayne/guess100/MainActivity.java
+++ app/src/main/java/com/wayne/guess100/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wayne.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,18 +8,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    Random random = new Random();
+    int n = random.nextInt(100)+1;
+    int min=1;
+    int max=100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bSend = (Button) findViewById(R.id.send);
     }
     public void Guess(View view){
+
         TextView secret = (TextView)findViewById(R.id.secret) ;
+        int s =Integer.parseInt(secret.getText().toString());
         TextView info = (TextView)findViewById(R.id.info);
-        EditText ednumber = (EditText)findViewById(R.id.number);
-        Button bsend = (Button) findViewById(R.id.send);
-        
+
+        EditText edNumber = (EditText)findViewById(R.id.number);
+        int number =Integer.parseInt(edNumber.getText().toString());
+
+        if(number<n){
+            min = number;
+            new AlertDialog.Builder(this)
+                    .setMessage("請輸入"+min+"~"+max)
+                    .setPositiveButton("OK",null)
+                    .show();
+        }else if (number>n){
+            max = number;
+            new AlertDialog.Builder(this)
+                    .setMessage("請輸入"+min+"~"+max)
+                    .setPositiveButton("OK",null)
+                    .show();
+        }else if (number==n){
+            new AlertDialog.Builder(this)
+                    .setMessage("答對了")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
+
     }
 }
+
